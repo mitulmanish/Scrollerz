dbff92c8573ef61116f520dbf7247212a168a550
mitul_manish <mitul.manish@gmail.com>
2018-08-04 17:31:56 +1000

    Initial Commit

---
+++ Scrollerz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scrollerz
+//
+//  Created by Mitul Manish on 4/8/18.
+//  Copyright © 2018 Mitul Manish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
